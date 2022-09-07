a67938640e08a368882ccf732c00fecbd10ce4e3
Mau <mdamiansalazar@gmail.com>
2022-09-06 21:42:11 -0300

    Tidied up code, basic triangle area calculator finished!

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,203 @@
 
 using namespace std;
 
+struct Line {
+    Vector2 point1;
+    Vector2 point2;
+};
+
+// -- Variables --
+
+// Ventana
 const int screenWidth = 800;
 const int screenHeight = 450;
 
-const int cantidadVectores = 4;
+// Vectores
+const int maxVectors = 4;
+Line vectors[maxVectors];
 
-struct Linea {
-    Vector2 punto1;
-    Vector2 punto2;
-};
-
-Linea vectores[cantidadVectores];
-float longitudes[cantidadVectores];
-
+// Intersecciones
 const int maxIntersects = 6;
 Vector2 intersections[maxIntersects];
 int currectIntersection;
+
+// -- Funciones --
+
+namespace Intersections {
+    // Añade una interseccion al array.
+    void Add(Vector2 intersection);
+    // Devuelve si la interseccion en cuestion ya se encuentra en el array.
+    bool IsRepeated(Vector2 intersection);
+    // Devuelve si la interseccion de una linea es invalida o si se encuentra fuera de rango.
+    bool IsInvalid(Line l, Vector2 intersection);
+    // Pasandole los datos de 2 lineas, encuentra la interseccion entre estas.
+    Vector2 LineLineIntersection(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4);
+    // Encuentra todas las posibles intersecciones, añade las que son validas.
+    void FindIntersections();
+}
+
+namespace Shapes {
+    void CalculateTriangle();
+    // Devuelve si la forma es un triangulo.
+    bool IsTriangle();
+    // Se ocupa de averiguar que forma es y hacer los calculos necesarios.
+    void Manage();
+}
+
+#pragma region Vectors
+// Devuelve la distancia entre 2 vectores.
+float GetDistance(Vector2 v1, Vector2 v2);
+// Devuelve el punto intermedio entre 2 vectores.
+Vector2 PointBetweenTwoVectors(Vector2 v1, Vector2 v2);
+
+void InitVectors();
+
+#pragma endregion
+
+int main() {
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(screenWidth, screenHeight, "Algebra");
+    SetTargetFPS(30);
+
+    int currentVector = 0;
+    bool par = true;
+    currectIntersection = 0;
+    InitVectors();
+
+    while (!WindowShouldClose()) {
+        // Input
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && currentVector < maxVectors) {
+            if (par) {
+                vectors[currentVector].point1 = GetMousePosition();
+                cout << "[" << currentVector + 1 << "] Punto 1: (" << vectors[currentVector].point1.x << ";" << vectors[currentVector].point1.y << ")" << endl;
+                par = !par;
+            }
+            else {
+                vectors[currentVector].point2 = GetMousePosition();
+                cout << "[" << currentVector + 1 << "] Punto 2: (" << vectors[currentVector].point2.x << ";" << vectors[currentVector].point2.y << ")" << endl;
+                par = !par;
+                currentVector++;
+            }
+            if (currentVector == maxVectors) {
+                Intersections::FindIntersections();
+                Shapes::Manage();
+            }
+        }
+        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+            currentVector = 0;
+            par = true;
+            currectIntersection = 0;
+            InitVectors();
+            cout << "Vectores reiniciados..." << endl;
+        }
+        // Update
+
+        // Draw
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        for (int i = 0; i < maxVectors; i++) {
+            if (i < currentVector) {
+                DrawLine(vectors[i].point1.x, vectors[i].point1.y, vectors[i].point2.x, vectors[i].point2.y, RED);
+            }
+            if (!par && currentVector < maxVectors) {
+                DrawLine(vectors[currentVector].point1.x, vectors[currentVector].point1.y, GetMousePosition().x, GetMousePosition().y, RED);
+            }
+        }
+        for (int i = 0; i < currectIntersection; i++) {
+            DrawCircle(intersections[i].x, intersections[i].y, 5.0f, GREEN);
+        }
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
+
+namespace Intersections {
+
+    void Add(Vector2 intersection) {
+        intersections[currectIntersection] = intersection;
+        currectIntersection++;
+    }
+
+    bool IsRepeated(Vector2 intersection) {
+        if (currectIntersection > 0) {
+            for (int i = 0; i < currectIntersection; i++) {
+                if (intersections[i].x == intersection.x && intersections[i].y == intersection.y) {
+                    return true;
+                }
+            }
+        }
+        else {
+            return false;
+        }
+        return false;
+    }
+
+    bool IsInvalid(Line l, Vector2 intersection) {
+        return (intersection.x < l.point1.x&& intersection.x < l.point2.x&& intersection.x
+            || intersection.x > l.point1.x&& intersection.x > l.point2.x && intersection.x
+            || intersection.y < l.point1.y&& intersection.y < l.point2.y&& intersection.y
+            || intersection.y > l.point1.y&& intersection.y > l.point2.y && intersection.y);
+    }
+
+    Vector2 LineLineIntersection(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
+        Vector2 intersection;
+
+        intersection.x =
+            (((x1 * y2 - y1 * x2) * (x3 - x4)) - ((x1 - x2) * (x3 * y4 - y3 * x4))) /
+            (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)));
+
+        intersection.y =
+            (((x1 * y2 - y1 * x2) * (y3 - y4)) - ((y1 - y2) * (x3 * y4 - y3 * x4))) /
+            (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)));
+
+        return intersection;
+    }
+
+    void FindIntersections() {
+        for (int i = 0; i < maxVectors; i++) {
+            for (int j = 0; j < maxVectors; j++) {
+                Vector2 intersect = LineLineIntersection(vectors[i].point1.x, vectors[i].point1.y, vectors[i].point2.x, vectors[i].point2.y, vectors[j].point1.x, vectors[j].point1.y, vectors[j].point2.x, vectors[j].point2.y);
+                if (!isnan(intersect.x) && !IsInvalid(vectors[i], intersect) && !IsInvalid(vectors[j], intersect) && !IsRepeated(intersect)) {
+                    Add(intersect);
+                    cout << "intersection found: (" << intersect.x << ";" << intersect.y << ")\n";
+                }
+            }
+        }
+    }
+
+}
+
+namespace Shapes {
+    void CalculateTriangle() {
+        float a = GetDistance(intersections[0], intersections[1]);
+        cout << "a: " << a << "\n";
+        float b = GetDistance(intersections[1], intersections[2]);
+        cout << "b: " << b << "\n";
+        float c = GetDistance(intersections[2], intersections[0]);
+        cout << "c: " << c << "\n";
+        float s = ((a + b + c) / 2);
+        float area = sqrt(s * ((s - a) * (s - b) * (s - c)));
+        cout << "The area of the triangle is: " << area << "\n";
+    }
+
+    bool IsTriangle() {
+        return (currectIntersection == 3);
+    }
+
+    void Manage() {
+        if (IsTriangle()) {
+            cout << "The shape is a triangle!\n";
+            CalculateTriangle();
+        }
+    }
+}
+
+#pragma region Vectors
 
 float GetDistance(Vector2 v1, Vector2 v2)
 {
@@ -37,143 +218,11 @@ Vector2 PointBetweenTwoVectors(Vector2 v1, Vector2 v2) {
     return point;
 }
 
-void CalculateTriangle() {
-    Vector2 point = PointBetweenTwoVectors(intersections[1], intersections[2]);
-    float triangleHeight = GetDistance(intersections[0], point);
-    cout << "The distance between (" << intersections[0].x << ";" << intersections[0].y << ") and (" << point.x << ";" << point.y << ") is: " << triangleHeight << endl;
-
-}
-
-bool IsTriangle() {
-    return (currectIntersection == 3);
-}
-
-void ShapeManager() {
-    if (IsTriangle)
-        CalculateTriangle();
-}
-
-
-#pragma region intersection
-
-void AddIntersection(Vector2 intersection) {
-    intersections[currectIntersection] = intersection;
-    currectIntersection++;
-}
-
-bool IsRepeated(Vector2 intersection) {
-    if (currectIntersection > 0) {
-        for (int i = 0; i < currectIntersection; i++) {
-            if (intersections[i].x == intersection.x && intersections[i].y == intersection.y) {
-                return true;
-            }
-        }
-    }
-    else {
-        return false;
-    }
-    return false;
-}
-
-bool OobIntersection(Linea l, Vector2 intersect) {
-    return (intersect.x < l.punto1.x && intersect.x < l.punto2.x && intersect.x
-        || intersect.x > l.punto1.x&& intersect.x > l.punto2.x&& intersect.x
-        || intersect.y < l.punto1.y&& intersect.y < l.punto2.y&& intersect.y
-        || intersect.y > l.punto1.y&& intersect.y > l.punto2.y&& intersect.y);
-}
-
-Vector2 LineLineIntersection(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
-    Vector2 intersection;
-
-    intersection.x =
-        (((x1 * y2 - y1 * x2) * (x3 - x4)) - ((x1 - x2) * (x3 * y4 - y3 * x4))) /
-        (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)));
-
-    intersection.y =
-        (((x1 * y2 - y1 * x2) * (y3 - y4)) - ((y1 - y2) * (x3 * y4 - y3 * x4))) /
-        (((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)));
-
-    return intersection;
-}
-
-void CompareAllLines() {
-    for (int i = 0; i < cantidadVectores; i++) {
-        for (int j = 0; j < cantidadVectores; j++) {
-            Vector2 intersect = LineLineIntersection(vectores[i].punto1.x, vectores[i].punto1.y, vectores[i].punto2.x, vectores[i].punto2.y, vectores[j].punto1.x, vectores[j].punto1.y, vectores[j].punto2.x, vectores[j].punto2.y);
-            if (!isnan(intersect.x) && !OobIntersection(vectores[i], intersect) && !OobIntersection(vectores[j], intersect) && !IsRepeated(intersect)) {
-                AddIntersection(intersect);
-                cout << "intersection found: (" << intersect.x << ";" << intersect.y << ")\n";
-            }
-        }
+void InitVectors() {
+    for (int i = 0; i < maxVectors; i++) {
+        vectors[i].point1 = { 0, 0 };
+        vectors[i].point2 = { 0, 0 };
     }
 }
 
 #pragma endregion
-
-void InitVectores() {
-    for (int i = 0; i < cantidadVectores; i++) {
-        vectores[i].punto1 = { 0, 0 };
-        vectores[i].punto2 = { 0, 0 };
-    }
-}
-
-int main() {
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "Algebra");
-    SetTargetFPS(30);
-
-    int vectorActual = 0;
-    bool par = true;
-    currectIntersection = 0;
-    InitVectores();
-
-    while (!WindowShouldClose()) {
-        // Input
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && vectorActual < cantidadVectores) {
-            if (par) {
-                vectores[vectorActual].punto1 = GetMousePosition();
-                cout << "[" << vectorActual + 1 << "] Punto 1: (" << vectores[vectorActual].punto1.x << ";" << vectores[vectorActual].punto1.y << ")" << endl;
-                par = !par;
-            }
-            else {
-                vectores[vectorActual].punto2 = GetMousePosition();
-                cout << "[" << vectorActual + 1 << "] Punto 2: (" << vectores[vectorActual].punto2.x << ";" << vectores[vectorActual].punto2.y << ")" << endl;
-                par = !par;
-                vectorActual++;
-            }
-            if (vectorActual == cantidadVectores) {
-                CompareAllLines();
-                ShapeManager();
-            }
-        }
-        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
-            vectorActual = 0;
-            currectIntersection = 0;
-            InitVectores();
-            cout << "Vectores reiniciados..." << endl;
-        }
-        // Update
-
-        // Draw
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        for (int i = 0; i < cantidadVectores; i++) {
-            if (i < vectorActual) {
-                DrawLine(vectores[i].punto1.x, vectores[i].punto1.y, vectores[i].punto2.x, vectores[i].punto2.y, RED);
-            }
-            if (!par && vectorActual < cantidadVectores) {
-                DrawLine(vectores[vectorActual].punto1.x, vectores[vectorActual].punto1.y, GetMousePosition().x, GetMousePosition().y, RED);
-            }
-        }
-        for (int i = 0; i < currectIntersection; i++) {
-            DrawCircle(intersections[i].x, intersections[i].y, 5.0f, GREEN);
-        }
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-
-    return 0;
-}
